149a7fb282f2b3454a64dee1f426b6ba19fe2c3b
shimanshu sharma <shimanshu.sharma@tothenew.com>
2022-10-06 09:38:09 +0530

    MultiThreading Exercise

---
+++ Question2.java
@@ -0,0 +1,12 @@
+package Source;
+
+//Use a singleThreadExecutor, newCachedThreadPool() and  newFixedThreadPool()
+// to submit a list of tasks and wait for completion of all tasks.
+
+public class Question2 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
